77e53893c9f612f84bec947fd5de9a414b5a1c52
cr7kawai <70409607+cr7kawai@users.noreply.github.com>
2023-02-15 10:18:51 -0600

    2 files modified

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,32 +1,34 @@
 import { Component } from '@angular/core';
-import { UsersService } from 'src/app/services/users.service';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-  email:string; //Captura de email
-  password:string;  //Captura de password
-  confirmPassword:string;
+  email: string; //Captura de email
+  password: string; //Captura de password
+  confirmPassword: string;
   passwordError: boolean;
 
-  constructor(public userService: UsersService) { //Inicializar los dos atributos
+  constructor(public userService: UsersService) {
+    //Inicializar los dos atributos
     this.email = '';
-    this.password = ''
+    this.password = '';
     this.confirmPassword = '';
     this.passwordError = false;
   }
 
-  register() {  //Función para evento clic
+  register() {
+    //Función para evento clic
     console.log('Email: ' + this.email);
     console.log('Password: ' + this.password);
     console.log('Confirm Password: ' + this.confirmPassword);
 
-    const user = {email: this.email, password:this.password};
+    const user = { email: this.email, password: this.password };
 
-    this.userService.register(user).subscribe(data => {
+    this.userService.register(user).subscribe((data) => {
       console.log(data);
     });
   }
